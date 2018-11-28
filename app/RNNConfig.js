7a2b735518f47b1121b25c23b7596a471ee77c52
jack-gu <528489389@qq.com>
2018-11-28 16:58:53 +0800

    -IOS适配-

--- app/RNNConfig.js
+++ app/RNNConfig.js
@@ -124,68 +124,64 @@ const thirdTab = {
 
 
 //定义APP的样式的样式，动画效果只对android有效果，firstSetRoot是整对android是不是启动动画而言的
-export const AppStyle = (firstSetRoot) => {
-    console.log({width: width * PixelRatio.get()});
-    return {
-        topBar: {
-            animate: true,
-            hideOnScroll: false,
-            buttonColor: 'black',
-            drawBehind: false,
-            title: {
-                text: 'title',
-                fontSize: 20,
-                color: AppConfig.COLOR_WHITE,
-                alignment: 'center',
-            },
-            subtitle: {
-                fontSize: AppConfig.TEXT_SIZE_SMALL,
-                color: AppConfig.TEXT_COLOR_GRAY,
-            },
-            background: {
-                color: AppConfig.COLOR_THEME,
-            },
-            backButton: {
-                color: AppConfig.COLOR_WHITE,
-            }
+export const AppStyle = {
+    topBar: {
+        animate: true,
+        hideOnScroll: false,
+        buttonColor: 'black',
+        drawBehind: false,
+        title: {
+            text: 'title',
+            fontSize: 20,
+            color: AppConfig.COLOR_WHITE,
+            alignment: 'center',
         },
-        layout: {
-            backgroundColor: AppConfig.COLOR_BG,
-            orientation: ['portrait'],
-        },
-        bottomTabs: {
-            backgroundColor: AppConfig.COLOR_WHITE,
-            titleDisplayMode: 'alwaysShow',
-            drawBehind: true,
-        },
-        bottomTab: {
+        subtitle: {
             fontSize: AppConfig.TEXT_SIZE_SMALL,
-            iconColor: AppConfig.TEXT_COLOR_GRAY,
-            selectedIconColor: AppConfig.COLOR_THEME,
-            textColor: AppConfig.TEXT_COLOR_GRAY,
-            selectedTextColor: AppConfig.COLOR_THEME,
-            selectedFontSize: AppConfig.TEXT_SIZE_SMALL,
+            color: AppConfig.TEXT_COLOR_GRAY,
         },
-        overlay: {
-            interceptTouchOutside: true
+        background: {
+            color: AppConfig.COLOR_THEME,
         },
-        topTabs: {
-            selectedTabColor: AppConfig.COLOR_THEME,
-            unselectedTabColor: AppConfig.TEXT_COLOR_GRAY,
-            height: 70,
-        },
-        animations: firstSetRoot ?
-            null : {
-                setRoot: {
-                    x: {
-                        from: width * PixelRatio.get(),
-                        to: 0,
-                        duration: 300,
-                        interpolation: 'accelerate'
-                    }
-                }
+        backButton: {
+            color: AppConfig.COLOR_WHITE,
+        }
+    },
+    layout: {
+        backgroundColor: AppConfig.COLOR_BG,
+        orientation: ['portrait'],
+    },
+    bottomTabs: {
+        backgroundColor: AppConfig.COLOR_WHITE,
+        titleDisplayMode: 'alwaysShow',
+        drawBehind: true,
+    },
+    bottomTab: {
+        fontSize: AppConfig.TEXT_SIZE_SMALL,
+        iconColor: AppConfig.TEXT_COLOR_GRAY,
+        selectedIconColor: AppConfig.COLOR_THEME,
+        textColor: AppConfig.TEXT_COLOR_GRAY,
+        selectedTextColor: AppConfig.COLOR_THEME,
+        selectedFontSize: AppConfig.TEXT_SIZE_SMALL,
+    },
+    overlay: {
+        interceptTouchOutside: true
+    },
+    topTabs: {
+        selectedTabColor: AppConfig.COLOR_THEME,
+        unselectedTabColor: AppConfig.TEXT_COLOR_GRAY,
+        height: 70,
+    },
+    animations: {
+        setRoot: {
+            x: {
+                from: width * PixelRatio.get(),
+                to: 0,
+                duration: 300,
+                interpolation: 'accelerate'
             }
-    };
+        }
+    }
 };
 
 export const LoginIndex = {
